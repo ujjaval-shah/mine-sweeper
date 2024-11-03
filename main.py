474d0a0a859c9a76ed4543ba37782f5d410fddf3
ujjaval-shah <uss1999int@gmail.com>
2024-11-03 16:57:39 +0530

    working on css 2

--- main.py
+++ main.py
@@ -6,13 +6,14 @@
 
 
 ### To experiment with CSS
-from pyscript.web import page, div, input_, button
+from pyscript.web import page, div, input_, button, img
 
 
 root_node = page["#root"][0]
 
-mines_field, time_field = input_(id="mines-field"), input_(id="time-field")
-restart_button = button(id="restart-button")
+mines_field = input_(id="mines-field", type="text", size="3", value="000")
+time_field = input_(id="time-field", type="text", size="3", value="000")
+restart_button = button(img(src=r".\assets\slightly-smiling-face.png"), id="restart-button")
 topbar = div(
     id="topbar",
     children=[time_field, restart_button, mines_field]
